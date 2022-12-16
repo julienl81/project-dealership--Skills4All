b2be4ba8b10ab86678c6af6967a3d95adcf08988
JulienLaurent77 <julienlaurent77@outlook.com>
2022-12-16 14:54:15 +0100

    installation of knp_paginator and add bootstrap style

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -4,17 +4,27 @@ namespace App\Controller;
 
 use App\Repository\CarCategoryRepository;
 use App\Repository\CarRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends AbstractController
 {
     #[Route('/car', name: 'app_car')]
-    public function index(CarRepository $carRepository, CarCategoryRepository $carCategorie): Response
+    public function index(CarRepository $carRepository, CarCategoryRepository $carCategorie, PaginatorInterface $paginator, Request $request): Response
     {
+
+        $cars = $carRepository->findAll();
+        $pagination = $paginator->paginate(
+            $cars,
+            $request->query->getInt('page', 1),
+            20
+        );
+
         return $this->render('car/index.html.twig', [
-            'cars' => $carRepository->findAll(),
+            'cars' => $pagination,
             'carCategories' => $carCategorie->findAll(),
         ]);
     }
