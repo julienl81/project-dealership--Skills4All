b9f8de03484f60478234b6f0c720986d741dc48e
JulienLaurent77 <julienlaurent77@outlook.com>
2022-12-16 11:31:57 +0100

    table list of all car showing with sucess (with bootstrap)

---
+++ src/Controller/CarController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CarRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarController extends AbstractController
+{
+    #[Route('/car', name: 'app_car')]
+    public function index(CarRepository $carRepository): Response
+    {
+
+        return $this->render('car/index.html.twig', [
+            'cars' => $carRepository->findAll(),
+        ]);
+    }
+}
