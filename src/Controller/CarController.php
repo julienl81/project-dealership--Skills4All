be86e3e6f844aed45cc26c4e67f6ff2efcf7b127
JulienLaurent77 <julienlaurent77@outlook.com>
2022-12-17 17:18:58 +0100

    search works

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -13,8 +13,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class CarController extends AbstractController
 {
     #[Route('/car/{carCategory}', name: 'app_car_category')]
-    public function carCategory($carCategory, CarRepository $carRepository, CarCategoryRepository $carCategorieRepository, PaginatorInterface $paginator, Request $request): Response
+    public function carList($carCategory, CarRepository $carRepository, CarCategoryRepository $carCategorieRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        //dd($request->query->get('search'));
+
         if ($carCategory != 'all') {
             $carCategoryId = $carCategorieRepository->findOneBy(['name' => $carCategory]);
             $cars = $carRepository->findByCarCategory($carCategoryId);
@@ -31,6 +33,20 @@ class CarController extends AbstractController
         return $this->render('car/index.html.twig', [
             'cars' => $pagination,
             'carCategories' => $carCategorieRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/car/all/search', name: 'app_car_search')]
+    public function search(Request $request, CarRepository $carRepository, CarCategoryRepository $carCategorieRepository): Response
+    {
+        //dd($request->query->get('search'));
+
+        $carSearch = $carRepository->findBy(['name' => $request->query->get('search')]);
+        //dd($carSearch);
+
+        return $this->render('car/search.html.twig', [
+            'cars' => $carSearch,
+            'carCategories' => $carCategorieRepository->findAll()
         ]);
     }
 }
